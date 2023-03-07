4e4cfac83ab5bceccf047c300dd4149fde795f1c
Georgi Ivanov <66921360+georgi-ivanov66@users.noreply.github.com>
2023-03-07 08:37:35 +0000

    Add Meals API

---
+++ APIs/MealsAPI/Controllers/MealsController.cs
@@ -0,0 +1,51 @@
+﻿using MealsAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+
+namespace MealsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MealsController : ControllerBase
+    {
+        // Cosmos DB details, In real use cases, these details should be configured in secure configuraion file.
+        private readonly string CosmosDBAccountUri = "https://fitness-track-cn.documents.azure.com:443/";
+        // host.docker.internal
+        private readonly string CosmosDBAccountPrimaryKey = "nET8oDlA3KtgqkIyYefoe78mABsbTcOQgwTtYCBFyNOJs4htUPh9DXKjLPJd4BgtR3Z4XSsXLg8fACDbcNSiUQ==";
+        private readonly string CosmosDbName = "FitnessTrack";
+        private readonly string CosmosDbContainerName = "Meals";
+
+        /// <summary>
+        /// Commom Container Client, you can also pass the configuration paramter dynamically.
+        /// </summary>
+        /// <returns> Container Client </returns>
+        private Container ContainerClient()
+        {
+
+            CosmosClient cosmosDbClient = new CosmosClient(CosmosDBAccountUri, CosmosDBAccountPrimaryKey);
+            Container containerClient = cosmosDbClient.GetContainer(CosmosDbName, CosmosDbContainerName);
+            return containerClient;
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(Meal meal)
+        {
+            try
+            {
+                
+                var container = ContainerClient();
+                ContainerProperties properties = await container.ReadContainerAsync();
+                Console.WriteLine(properties.PartitionKeyPath);
+                var response = await container.CreateItemAsync(meal, new PartitionKey(meal.applicationUserId));
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ APIs/MealsAPI/Models/Meal.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealsAPI.Models
+{
+    public class Meal
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid Id { get; set; }
+        public string applicationUserId { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public double TotalCalories { get; set; }
+        public double Protein { get; set; }
+        public double Fats { get; set; }
+        public double Carbohydrates { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
